0d9331428c358125141cc7fd16212493cbe9ab76
Quba1 <jak.lewand@gmail.com>
2021-07-30 21:24:47 +0200

    finish and cleanup of constructors

--- src/errors.rs
+++ src/errors.rs
@@ -1,15 +1,22 @@
+use errno::Errno;
 use thiserror::Error;
 
-#[derive(Copy, Clone, Error, Debug)]
+#[derive(Clone, Error, Debug)]
 pub enum CodesError {
-    #[error("Internal ecCodes error occured")]
-    Internal,
+    #[error("Internal ecCodes error occured with code {0}")]
+    Internal(i32),
     #[error("Internal libc error occured")]
     Libc(#[from] LibcError),
 }
 
-#[derive(Copy, Clone, Error, Debug)]
+#[derive(Clone, Error, Debug)]
 pub enum LibcError {
-    #[error("Internal libc error occured")]
-    NullPtr,
+    #[error("Libc function returned null pointer, errno code {0} with error {0}")]
+    NullPtr(i32, Errno),
+
+    #[error("Libc function returned non-zero code")]
+    NonZero,
+
+    #[error(transparent)]
+    CStringNull(#[from] std::ffi::NulError),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //!# Unofficial high-level safe Rust bindings to ecCodes library.
 //!
-//!**This crate is in early stage of development.** First usable version will be released soon.
+//!**Currently only reading of GRIB files is supported.**
 //!
 //!Check README for more details and how to contribute.
 //!
@@ -18,92 +18,185 @@
 
 mod errors;
 
-use eccodes_sys::{self, _IO_FILE, codes_context, codes_handle};
-use libc::{FILE, size_t, c_void, c_char};
-use errors::{CodesError, LibcError};
+use std::ffi::CString;
+
 use bytes::Bytes;
+use eccodes_sys::{
+    self, codes_context, codes_handle, ProductKind_PRODUCT_GRIB, _IO_FILE,
+};
+use errno::errno;
+use errors::{CodesError, LibcError};
+use libc::{c_char, c_void, size_t, FILE};
+use log::error;
 
 ///Enum indicating what type of product `CodesHandle` is currently holding.
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub enum ProductKind {
     GRIB,
-    BUFR,
 }
 
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 enum DataContainer {
     FileBytes(Bytes),
-    FileBuffer(String)
+    FileBuffer(String),
 }
 
 ///Main structure used to operate on the GRIB/BUFR file.
 ///It takes a full ownership of the accessed file.
 ///It can be constructed either using a file (with `fopen()`) or memory buffer (with `fmemopen()`).
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct CodesHandle {
     ///The container to take the ownership of handled file
     data: DataContainer,
     ///Internal ecCodes unsafe handle
-    handle: *mut codes_handle,
-    product_kind: ProductKind,
+    file_handle: *mut codes_handle,
+    file_pointer: *mut FILE,
+    product_kind: u32,
 }
 
 impl CodesHandle {
-    pub fn new_from_memory(file_data: Bytes, product_kind: ProductKind) -> Result<Self, CodesError> {
-        let file_handle = open_with_fmemopen(&file_data)?;
-        let handle = CodesHandle::codes_grib_handle_new_from_file(file_handle)?;
+    pub fn new_from_memory(
+        file_data: Bytes,
+        product_kind: ProductKind,
+    ) -> Result<Self, CodesError> {
+        let product_kind = match_product_kind(product_kind);
+
+        let file_pointer = open_with_fmemopen(&file_data)?;
+        let file_handle = CodesHandle::codes_handle_new_from_file(file_pointer, product_kind)?;
 
         Ok(CodesHandle {
             data: (DataContainer::FileBytes(file_data)),
-            handle,
+            file_handle,
+            file_pointer,
             product_kind,
         })
     }
 
-    fn codes_grib_handle_new_from_file(mem_handle: *mut FILE) -> Result<*mut codes_handle, CodesError> {
+    pub fn new_from_file(file_name: String, product_kind: ProductKind) -> Result<Self, CodesError> {
+        let product_kind = match_product_kind(product_kind);
+
+        let file_pointer = open_with_fopen(file_name.clone())?;
+        let file_handle = CodesHandle::codes_handle_new_from_file(file_pointer, product_kind)?;
+
+        Ok(CodesHandle {
+            data: (DataContainer::FileBuffer(file_name)),
+            file_handle,
+            file_pointer,
+            product_kind,
+        })
+    }
+
+    fn codes_handle_new_from_file(
+        file_pointer: *mut FILE,
+        product_kind: u32,
+    ) -> Result<*mut codes_handle, CodesError> {
         let context: *mut codes_context = std::ptr::null_mut(); //default context
 
-        let grib_handle;
-        let mut error: i32 = 0;
+        let file_handle;
+        let mut error_code: i32 = 0;
         unsafe {
-            grib_handle =
-                eccodes_sys::codes_grib_handle_new_from_file(context, mem_handle as *mut _IO_FILE, &mut error as *mut i32);
+            file_handle = eccodes_sys::codes_handle_new_from_file(
+                context,
+                file_pointer as *mut _IO_FILE,
+                product_kind,
+                &mut error_code as *mut i32,
+            );
         }
 
-        if error != 0 {
-            return Err(CodesError::Internal);
+        if error_code != 0 {
+            return Err(CodesError::Internal(error_code));
         }
 
-        Ok(grib_handle)
+        Ok(file_handle)
     }
 }
 
 impl Drop for CodesHandle {
-    fn drop (&mut self) {
+    ///Executes the desctructor for this type ([read more](https://doc.rust-lang.org/1.54.0/core/ops/drop/trait.Drop.html#tymethod.drop)).
+    ///This method calls `codes_handle_delete()` from ecCodes and `fclose()` from libc for graceful cleanup.\
+    ///**WARNING:** Currently it is assumed that under normal circumstances this destructor never fails.
+    ///However in some edge cases ecCodes or fclose can return non-zero code.
+    ///For now user is informed about that through log, because I don't know how to handle it correctly.
+    ///If some bugs occurs during drop please enable log output and post issue on Github.
+    fn drop(&mut self) {
         let error_code;
-
         unsafe {
-            error_code = eccodes_sys::codes_handle_delete(self.handle);
+            error_code = eccodes_sys::codes_handle_delete(self.file_handle);
         }
-    
+
         if error_code != 0 {
-            panic!("CodesHandle destructor failed with ecCodes error code {:?}!", error_code);
+            error!(
+                "CodesHandle destructor failed with ecCodes error code {:?}",
+                error_code
+            );
         }
+
+        let error_code;
+        unsafe {
+            error_code = libc::fclose(self.file_pointer);
+        }
+
+        if error_code != 0 {
+            let error_val = errno();
+            let code = error_val.0;
+            error!(
+                "CodesHandle destructor failed with libc error {}, code: {}",
+                code, error_val
+            );
+        }
+
+        todo!(
+            "Review required! Destructor is assumed to never fail under normal circumstances. 
+        However in some edge cases ecCodes or fclose can return non-zero code. 
+        For now user is informed about that through log, 
+        because I don't know how to handle it correctly."
+        );
     }
 }
 
-fn open_with_fmemopen(grib_file: &Bytes) -> Result<*mut FILE, LibcError> {
-    let grib_size = grib_file.len() as size_t;
-    let grib_mode = "r".as_ptr().cast::<c_char>();
+fn match_product_kind(product_kind: ProductKind) -> u32 {
+    let product_kind = match product_kind {
+        ProductKind::GRIB => ProductKind_PRODUCT_GRIB,
+    };
 
-    let grib_ptr = grib_file.as_ptr() as *mut c_void;
+    product_kind
+}
 
-    let grib_obj;
+fn open_with_fmemopen(file_data: &Bytes) -> Result<*mut FILE, LibcError> {
+    let file_size = file_data.len() as size_t;
+    let open_mode = "r".as_ptr().cast::<c_char>();
+
+    let file_ptr = file_data.as_ptr() as *mut c_void;
+
+    let file_obj;
     unsafe {
-        grib_obj = libc::fmemopen(grib_ptr, grib_size, grib_mode);
+        file_obj = libc::fmemopen(file_ptr, file_size, open_mode);
     }
 
-    if grib_obj.is_null() {
-        return Err(LibcError::NullPtr);
+    if file_obj.is_null() {
+        let error_val = errno();
+        let error_code = error_val.0;
+        return Err(LibcError::NullPtr(error_code, error_val));
     }
 
-    Ok(grib_obj)
+    Ok(file_obj)
+}
+
+fn open_with_fopen(file_name: String) -> Result<*mut FILE, LibcError> {
+    let open_mode = "r".as_ptr().cast::<c_char>();
+    let file_name = CString::new(file_name)?;
+    let filename_ptr = file_name.as_ptr();
+
+    let file_obj;
+    unsafe {
+        file_obj = libc::fopen(filename_ptr, open_mode);
+    }
+
+    if file_obj.is_null() {
+        let error_val = errno();
+        let error_code = error_val.0;
+        return Err(LibcError::NullPtr(error_code, error_val));
+    }
+
+    Ok(file_obj)
 }
